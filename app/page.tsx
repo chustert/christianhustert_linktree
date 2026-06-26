0408d019143d0946e3ba6b60fbdc8fdf6b61e859
Christian Hustert <chris@hndrx.co.nz>
2026-06-27 00:03:12 +1200

    Update links in page.tsx to include new entries for CH1337 and GitHub profile

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,16 @@ const links = [
     href: "https://newgameday.com/",
   },
   {
+    label: "CV, XP & Log on CH1337",
+    href: "https://ch1337.com/",
+  },
+  {
     label: "Building dope tech at Scanabull",
     href: "https://scanabull.com/",
+  },
+  {
+    label: "Showing off my coding skills on GitHub",
+    href: "https://github.com/chustert",
   },
   {
     label: "Pretending to do networking on LinkedIn",
